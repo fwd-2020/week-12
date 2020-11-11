f3996160db9879ec2ffa7966aad3df6a0da318ac
Karl Stolley <karl.stolley@gmail.com>
2020-11-11 16:08:16 -0600

    Demonstrate a named callback function

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,13 @@
 console.log("I am JavaScript. HEAR ME ROAR!");
 
+function omgClicked(event) {
+  event.target.innerText = 'OMG you know what my target was, from a function!';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var heading = document.querySelector('h1');
   heading.innerText = "Hello from DOM-Ready JavaScript!";
 
-  heading.addEventListener('click', function() {
-    heading.innerText = "OMG you just clicked me!!!";
-  })
+  heading.addEventListener('click', omgClicked);
 
 });
