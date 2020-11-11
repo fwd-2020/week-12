0ae9064acc95ae358d9af9eca3d17aae44f056d3
Karl Stolley <karl.stolley@gmail.com>
2020-11-11 16:19:19 -0600

    Tuck all JavaScript into an IIFE

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,25 +1,25 @@
 // IIFE: Immediately Invoked Function Expression
+// This keeps ALL of our custom functions and
+// variables OUT of the global scope (that is,
+// they will not permanently attach to the window
+// object):
 
 (function() {
   console.log("I am immediately invoked!");
-  var fruit = "Apple";
-  var alert = "OMG RUN!!!";
-  alert('Hey, here is an alert');
+
+  console.log("I am JavaScript. HEAR ME ROAR!");
+
+  function omgClicked(event) {
+    event.target.innerText = 'OMG you know what my target was, from a function!';
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var heading = document.querySelector('h1');
+    heading.innerText = "Hello from DOM-Ready JavaScript!";
+
+    heading.addEventListener('click', omgClicked);
+
+  });
+
 })();
-
-var vegetable = "Carrot";
-
-
-console.log("I am JavaScript. HEAR ME ROAR!");
-
-function omgClicked(event) {
-  event.target.innerText = 'OMG you know what my target was, from a function!';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var heading = document.querySelector('h1');
-  heading.innerText = "Hello from DOM-Ready JavaScript!";
-
-  heading.addEventListener('click', omgClicked);
-
-});
