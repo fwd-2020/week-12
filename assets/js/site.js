4eb113f83d98bf0bf2872882df1ff354f3585971
Karl Stolley <karl.stolley@gmail.com>
2020-11-11 16:00:55 -0600

    Handle the DOMContentLoaded event, and clicks on heading

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,11 @@
 console.log("I am JavaScript. HEAR ME ROAR!");
+
+document.addEventListener('DOMContentLoaded', function() {
+  var heading = document.querySelector('h1');
+  heading.innerText = "Hello from DOM-Ready JavaScript!";
+
+  heading.addEventListener('click', function() {
+    heading.innerText = "OMG you just clicked me!!!";
+  })
+
+});
